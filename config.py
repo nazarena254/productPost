cd7ec038bf26a9ecf8a484ae6e8dd4db74f3718b
MugeraH <bwosihughes24@gmail.com>
2021-06-16 13:43:06 +0300

    file setup

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+ 
+    # API_KEY = os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mugera:Mugbwo9856@localhost/watch2_test'
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mugera:Mugbwo9856@localhost/currency'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
