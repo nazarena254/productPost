0e6c8eabe0a9b0437e6e7403c5925394645bd76c
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-12 20:36:26 +0300

    add list of available commands in manage.py file

--- manage.py
+++ manage.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from app import create_app
-# from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
 # from app.models import User
 
-
+# Use "python manage.py" for a list of available commands.
 # Creating app instance
 app = create_app('development')  # during development stage
 #app = create_app('production')  # during production/deployment to heroku stage
 # app = create_app('test')
 
-#manager = Manager(app)
-#manager.add_command('server',Server)
- 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
-
+#Manager class keeps track of all the commands  
+# and handles how they are called from the command line
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+migrate.add_command('server',Server)
 
 # @manager.command
 # def test():
@@ -25,9 +25,9 @@ app = create_app('development')  # during development stage
 #     tests = unittest.TestLoader().discover('tests')
 #     unittest.TextTestRunner(verbosity=2).run(tests)
 
-#@manager.shell
+@manager.shell
 def make_shell_context():
-    return dict(app = app)
+    return dict(app = app, db=db)
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
     manager.run()
