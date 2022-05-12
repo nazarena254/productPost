0235146785d092bf0db8ce0289e5ae29e6f425a7
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-12 20:56:30 +0300

    add and export db_url and secret_key in start.sh file

--- config.py
+++ config.py
@@ -3,22 +3,22 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
 
-
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
 
 class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://nancyngunjiri1@gmail.com:nazarenah333@localhost:5432/product'
     pass
 
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL')
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-# 'test':TestConfig
+'test':TestConfig
 }
